5710dffbfd7722c99c4982e2a1e2253c61a9ac3f
Xieyuschen <Xieyuschen@users.noreply.github.com>
2020-03-23 14:11:59 +0800

    Add  comment about pointer|value-like class type

--- Oop_pra/TreeNode.cpp
+++ Oop_pra/TreeNode.cpp
@@ -7,14 +7,24 @@ using namespace std;
 class TreeNode
 {
 public:
-	TreeNode() :value(""), count(1), left(nullptr), right(nullptr) {};
-	TreeNode(const string& s = string(), TreeNode* l = nullptr, TreeNode* r = nullptr)
-		:value(s), left(l), right(r), count(1) {};
-	~TreeNode();
+	TreeNode() : value(string()), count(new int(1)), left(nullptr), right(nullptr) { }
+	TreeNode(const TreeNode& rhs) : value(rhs.value), count(rhs.count), left(rhs.left), right(rhs.right) { ++* count; }
+
+	//写拷贝构造函数的时候，我需要分析这个类是有类值行为还是有类指针行为
+	//对于节点来说，我希望它具有指针行为，如果具有值行为的话，也就是每次拷贝都给它的指针进行拷贝
+
+	~TreeNode() {
+		if (-- * count == 0) {
+			delete left;
+			delete right;
+			delete count;
+		}
+	}
 
 private:
 	string value;
-	int count;
+	//必须保证所有的拷贝都指向一个地方，那么指针理所当然必须要
+	int *count;
 	TreeNode* left;
 	TreeNode* right;
 };
