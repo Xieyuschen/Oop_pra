59a67b1da0dce2a6d0baccb85f566fbb6b073a4f
Xieyuschen <Xieyuschen@users.noreply.github.com>
2020-04-02 19:16:19 +0800

    finish basket by myself with suspecting about my code quantity

--- Oop_pra/Basket.h
+++ Oop_pra/Basket.h
@@ -3,8 +3,10 @@
 #include<map>
 #include<set>
 #include<memory>
+#include<iostream>
+#include"Bulk_quote.h"
+#include"disc_quote.h"
 #include"Quote.h"
-#include"SelfQuote.h"
 //智能指针在memory里面
 class Basket
 {
@@ -13,6 +15,14 @@ public:
 	~Basket();
 	double add_item(const std::shared_ptr<Quote>& sale) {
 		items.insert(sale);
+	}
+	double total_receipt()const {
+		double ret = 0.0;
+		for (auto a : items) {
+			ret += a->net_price;
+		}
+		std::cout << "The total in Basket are: " << ret << endl;
+		return ret;
 	}
 private:
 	//std::multimap < std::shared_ptr<Quote>, decltype()
